fe9eb7f3488f7de3455dad36ab32ddc5740a068a
Yasushis <viniciusmiura1@gmail.com>
2025-04-21 18:12:51 -0300

    add: contagem de tarefas

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ inputDescricao = document.getElementById("input-descricao")
 let buttonAdd = document.getElementById("button-tarefa")
 let main = document.getElementById("tarefas-lista")
 let contador = 0
+let total_tarefas = 0
+let tarefas_feitas = 0
+let tarefas_pendentes = 0
 
 function addTarefa() {
 
@@ -12,6 +15,10 @@ function addTarefa() {
     if (((valor !== null) && (valor !== undefined) && (valor !== "")) && ((valordesc !== null) && (valordesc !== undefined) && (valordesc !== "")))  {
 
         ++ contador
+        ++total_tarefas
+        ++tarefas_pendentes
+
+        document.getElementById("tarefinhas").innerHTML = `Tarefas: ${total_tarefas} <br> Tarefas feitas: ${tarefas_feitas} <br> Tarefas pendentes: ${tarefas_pendentes}`
 
         let novaTarefa = `<div class="div-tarefa" id="${contador}">
 
@@ -42,6 +49,7 @@ function addTarefa() {
     main.innerHTML += novaTarefa
     inputUsuario.value = ""
     inputUsuario.focus()
+    inputDescricao.value = ""
 
     }
 }
@@ -49,8 +57,17 @@ function addTarefa() {
 function deletarTarefa(id) {
 
     var tarefa = document.getElementById(id)
-    tarefa.remove()
+    --total_tarefas
+    var classe = tarefa.getAttribute("class")
 
+    if (classe == "div-tarefa") {
+        --tarefas_pendentes
+    }
+    else {
+        --tarefas_feitas
+    }
+    tarefa.remove()
+    document.getElementById("tarefinhas").innerHTML = `Tarefas: ${total_tarefas} <br> Tarefas feitas: ${tarefas_feitas} <br> Tarefas pendentes: ${tarefas_pendentes}`
 }
 
 function marcarTarefa(id) {
@@ -62,6 +79,10 @@ function marcarTarefa(id) {
 
         item.classList.add('div-tarefa-feita')
 
+        --tarefas_pendentes
+        ++tarefas_feitas
+        document.getElementById("tarefinhas").innerHTML = `Tarefas: ${total_tarefas} <br> Tarefas feitas: ${tarefas_feitas} <br> Tarefas pendentes: ${tarefas_pendentes}`
+
         var icone = document.getElementById("icone_" + id)
         icone.classList.remove('mdi-circle-outline')
         icone.classList.add('mdi-check-circle')
@@ -71,6 +92,10 @@ function marcarTarefa(id) {
     else {
 
         item.classList.remove('div-tarefa-feita')
+
+        --tarefas_feitas
+        ++tarefas_pendentes
+        document.getElementById("tarefinhas").innerHTML = `Tarefas: ${total_tarefas} <br> Tarefas feitas: ${tarefas_feitas} <br> Tarefas pendentes: ${tarefas_pendentes}`
 
         var icone = document.getElementById("icone_" + id)
         icone.classList.remove('mdi-check-circle')
